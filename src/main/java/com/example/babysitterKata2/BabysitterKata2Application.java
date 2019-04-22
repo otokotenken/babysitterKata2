feb4dcbc578a47f4022ea01d5ae7cc7f2cfeba4c
otokotenken <anna.sampson@detroitlabs.com>
2019-04-22 11:25:45 -0400

    original commit

---
+++ src/main/java/com/example/babysitterKata2/BabysitterKata2Application.java
@@ -0,0 +1,13 @@
+package com.example.babysitterKata2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabysitterKata2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabysitterKata2Application.class, args);
+	}
+
+}
