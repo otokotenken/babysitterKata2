4ebd512318cbf25bcb868d4c55409098d3759dd7
otokotenken <anna.sampson@detroitlabs.com>
2019-04-22 20:07:09 -0400

    happy path first test pass also included verfiy

--- src/main/java/com/example/babysitterKata2/BabysitterKataController.java
+++ src/main/java/com/example/babysitterKata2/BabysitterKataController.java
@@ -1,4 +1,18 @@
 package com.example.babysitterKata2;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BabysitterKataController {
+
+    @RequestMapping(value = "job", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> validateJob() {
+        return new ResponseEntity<>("Job Accepted", HttpStatus.ACCEPTED) ;
+    }
 }
--- src/test/java/com/example/babysitterKata2/BabysitterKataControllerTest.java
+++ src/test/java/com/example/babysitterKata2/BabysitterKataControllerTest.java
@@ -2,6 +2,13 @@ package com.example.babysitterKata2;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.http.MediaType;
@@ -9,9 +16,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BabysitterKataControllerTest {
 
     MockMvc mvc;
+
+    @Mock
+    BabysitterKataController babysitterKataController;
 
     @Before
     public void setUp(){
@@ -21,9 +32,12 @@ public class BabysitterKataControllerTest {
     @Test
     public void validateJobEndpointReturnsAnJobAcceptedMessage() throws Exception {
         String request = "";
+        Mockito.when(babysitterKataController.validateJob()).thenReturn(new ResponseEntity<>("Job Accepted", HttpStatus.ACCEPTED));
         mvc.perform(post("/job").contentType(MediaType.APPLICATION_JSON_UTF8_VALUE).content(request))
                 .andExpect(status().isAccepted())
                 .andExpect(content().string("Job Accepted"));
+        Mockito.verify(babysitterKataController, Mockito.times(1)).validateJob();
+        Mockito.verifyNoMoreInteractions(babysitterKataController);
     }
 
 }
