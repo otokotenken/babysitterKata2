20c7a874e5fcb5ef1c84cabe97fa48aa8b8e552c
otokotenken <anna.sampson@detroitlabs.com>
2019-04-23 20:49:42 -0400

    first red

--- src/main/java/com/example/babysitterKata2/BabysitterKataService.java
+++ src/main/java/com/example/babysitterKata2/BabysitterKataService.java
@@ -1,5 +1,8 @@
 package com.example.babysitterKata2;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BabysitterKataService {
 
 
--- src/test/java/com/example/babysitterKata2/BabysitterKataServiceTest.java
+++ src/test/java/com/example/babysitterKata2/BabysitterKataServiceTest.java
@@ -1,7 +1,25 @@
 package com.example.babysitterKata2;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
 import static org.junit.Assert.*;
 
 public class BabysitterKataServiceTest {
+
+    @InjectMocks
+    BabysitterKataService babysitterKataService;
+
+    Family familyA;
+    @Before
+    public void setUp() {
+        familyA = new Family(5, 11, 15);
+    }
+
+    @Test
+    public void ifStartTimeIsLessThanOrEqualTwelveSubtractFive() {
+        assertEquals(7, babysitterKataService.convertTimeToAPostiveRange(familyA.payShiftStartTime));
+    }
 
 }
